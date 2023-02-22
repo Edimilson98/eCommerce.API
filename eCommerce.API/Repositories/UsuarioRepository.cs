37a19ac09880be9d692ac62e047a19a97a125c9b
Edimilson98 <edimilsontl98@gmail.com>
2023-02-21 21:48:59 -0300

    Adicionar arquivos de projeto.

---
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,105 @@
+﻿using eCommerce.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+ * Connection => Estabelecer conexão com banco.
+ * Command => INSERT, UPDATE, DELETE.
+ * DataReader => Arq. Conectada. SELECT.
+ * DataAdapter => Arq. Desconectada. SELECT.
+ */
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private IDbConnection _connection;
+        public UsuarioRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+
+       
+        public List<Usuario> Get()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT * FROM Usuarios";
+                command.Connection = (SqlConnection)_connection;
+
+                _connection.Open();
+
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Usuario usuario = new Usuario();
+                    usuario.Id = dataReader.GetInt32("Id");
+                    usuario.Nome = dataReader.GetString("Nome");
+                    usuario.Email = dataReader.GetString("Email");
+                    usuario.Sexo = dataReader.GetString("Sexo");
+                    usuario.RG = dataReader.GetString("RG");
+                    usuario.CPF = dataReader.GetString("CPF");
+                    usuario.NomeMae = dataReader.GetString("NomeMae");
+                    usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                    usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                    usuarios.Add(usuario);
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return usuarios;
+        }
+
+        public Usuario Get(int id)
+        {
+            return _db.FirstOrDefault(a => a.Id == id);
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            var ultimoUsuario = _db.LastOrDefault();
+
+            if(ultimoUsuario == null)
+            {
+                usuario.Id = 1;
+            }
+            else
+            {
+                usuario.Id = ultimoUsuario.Id;
+                usuario.Id++;
+            }
+            _db.Add(usuario);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(_db.FirstOrDefault(a => a.Id == usuario.Id));
+            _db.Add(usuario);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(_db.FirstOrDefault(a => a.Id == id));
+        }
+
+
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario() { Id=1, Nome="Filipe Rodrigues", Email="filipe.rodrigues@gmail.com" },
+            new Usuario() { Id=2, Nome="Marcelo Rodrigues", Email="marcelo.rodrigues@gmail.com" },
+            new Usuario() { Id=3, Nome="Marcela Rodrigues", Email="marcela.rodrigues@gmail.com" }
+        };
+    }
+}
